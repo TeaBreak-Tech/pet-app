8f0b9112f7308b1ecc0e30747de50a88878db29c
Deslate <deslate@outlook.com>
2020-05-24 19:05:32 +0800

    finish a template for setting screen

---
+++ src/appearance/styles/style-settings.js
@@ -0,0 +1,41 @@
+import { StyleSheet } from 'react-native';
+
+var style_profile_card= StyleSheet.create({
+    item_height_container:{
+        backgroundColor:"white",
+        height:50,
+        borderTopWidth:4,
+        
+    },
+    item_width_container:{
+        marginLeft:30,
+        marginRight:22,
+        flex:1,
+        flexDirection:"row",
+        alignItems:"center",
+        justifyContent:"space-between",
+    },
+    title_text:{
+        fontSize: 16,
+        lineHeight: 23,
+        color: "#464646",
+    },
+    edit_indicator:{
+        height:40,
+        width:70,
+        alignItems:"flex-end",
+        justifyContent:"center"
+    },
+    // 临时的...作为icon
+    edit_indicator_icon:{
+        color:"#a5a5a5",
+        fontSize:30,
+        top:-7
+    },
+    space:{
+        height:30
+    }
+});
+
+export default style_profile_card
+
--- src/screens/settings-screen.js
+++ src/screens/settings-screen.js
@@ -4,10 +4,18 @@ import {
     View,
     Text,
     StyleSheet,
+    ScrollView,
+    FlatList,
+    TouchableOpacity,
+    Switch,
 } from "react-native";
 
 // Context
 import { connect } from 'react-redux'
+import {ThemeContext} from '../appearance/theme/theme-context-provider'
+
+// Style
+import style from '../appearance/styles/style-settings'
 
 // 从 Redux 获取 state 的方法 （用于获取登陆状态以决定是否添加登出键）
 const mapStateToProps = (state) => {
@@ -20,28 +28,111 @@ const mapStateToProps = (state) => {
 
 // 输出主页面
 // 将获取state的方法和dispatch的方法与输出类关联
-class SettingScreen extends Component {
+function SettingsScreen(){
+    const [alarm, setAlarm] = React.useState(false);
+    const [theme_name, setThemeName] = React.useState(true);
 
-    render() {
+    return (
+        <ThemeContext.Consumer>
+            {theme=>
+            <ScrollView>
+                <View style={[style.item_height_container,{backgroundColor:theme.background,borderTopColor:theme.background_emphasis}]}>
+                    <View style={style.item_width_container}>
+                        <Text style={style.title_text}>
+                            账号信息
+                        </Text>
+                        <TouchableOpacity style={style.edit_indicator}>
+                            <Text style={style.edit_indicator_icon}>...</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <View style={[style.item_height_container,{backgroundColor:theme.background,borderTopColor:theme.background_emphasis}]}>
+                    <View style={style.item_width_container}>
+                        <Text style={style.title_text}>
+                            登出/切换账号
+                        </Text>
+                        <TouchableOpacity style={style.edit_indicator}>
+                            <Text style={style.edit_indicator_icon}>...</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <View style={style.space}></View>
+                <View style={[style.item_height_container,{backgroundColor:theme.background,borderTopColor:theme.background_emphasis}]}>
+                    <View style={style.item_width_container}>
+                        <Text style={style.title_text}>
+                            隐私设置
+                        </Text>
+                        <TouchableOpacity style={style.edit_indicator}>
+                            <Text style={style.edit_indicator_icon}>...</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <View style={[style.item_height_container,{backgroundColor:theme.background,borderTopColor:theme.background_emphasis}]}>
+                    <View style={style.item_width_container}>
+                        <Text style={style.title_text}>
+                            消息提醒
+                        </Text>
+                        <Switch
+                            value={alarm}
+                            trackColor={{true:theme.basic}}
+                            onValueChange={()=>{setAlarm(!alarm)}}
+                        ></Switch>
+                    </View>
+                </View>
+                <View style={[style.item_height_container,{backgroundColor:theme.background,borderTopColor:theme.background_emphasis}]}>
+                    <View style={style.item_width_container}>
+                        <Text style={style.title_text}>
+                            显示模式
+                        </Text>
+                        <View style={{flexDirection:"row",alignItems:"center",justifyContent:"center"}}>
+                            <Text style={[style.title_text,{marginRight:10}]}>{theme_name?"白天":"黑夜"}</Text>
+                            <Switch
+                                value={theme_name}
+                                trackColor={{true:theme.background_emphasis}}
+                                onValueChange={()=>{setThemeName(!theme_name)}}
+                            ></Switch>
+                        </View>
+                    </View>
+                </View>
+                <View style={[style.item_height_container,{backgroundColor:theme.background,borderTopColor:theme.background_emphasis}]}>
+                    <View style={style.item_width_container}>
+                        <Text style={style.title_text}>
+                            缓存管理
+                        </Text>
+                        <TouchableOpacity style={style.edit_indicator}>
+                            <Text style={style.edit_indicator_icon}>...</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <View style={style.space}></View>
+                <View style={[style.item_height_container,{backgroundColor:theme.background,borderTopColor:theme.background_emphasis}]}>
+                    <View style={style.item_width_container}>
+                        <Text style={style.title_text}>
+                            联系客服
+                        </Text>
+                        <TouchableOpacity style={style.edit_indicator}>
+                            <Text style={style.edit_indicator_icon}>...</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <View style={[style.item_height_container,{backgroundColor:theme.background,borderTopColor:theme.background_emphasis}]}>
+                    <View style={style.item_width_container}>
+                        <Text style={style.title_text}>
+                            版本信息
+                        </Text>
+                        <TouchableOpacity style={style.edit_indicator}>
+                            <Text style={style.edit_indicator_icon}>...</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </ScrollView>
+            }
+        </ThemeContext.Consumer>
         
-        const { text,isLogin,path } = this.props;
-
-        return (
-        <View style={StyleSheet.container}>
-            <Text>
-                Hello ! This is Pedia ! Welcom: {text ||'no text'}
-            </Text>
-            <Text>
-                You are in navigation: {path ||'no path'}
-            </Text>
-            <Text>
-                Login Status: {isLogin?"Yes":"Not Log In"}
-            </Text>
-        </View>
-    );}
+    );
 }
 
-export default connect (mapStateToProps)(SettingScreen)
+export default connect (mapStateToProps)(SettingsScreen)
 
 
 
